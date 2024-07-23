88872534df379a303e027b7a161c390c9bc59f2d
Misha Oliinyk <380660775646a@gmail.com>
2024-07-23 13:39:08 +0300

    Refactor articleController by incapsulating async functions

--- src/controllers/articleController.ts
+++ src/controllers/articleController.ts
@@ -1,27 +1,21 @@
-import {Request, Response} from 'express';
 const Article = require("./../models/Article");
-import APIFeatures from "../middlewares/queryMiddleware";
-import * as mongoose from "mongoose";
 
-const createArticle = async (req:Request, res:Response): Promise<void> => {
-  try {
+import APIFeatures from "../middlewares/queryMiddleware";
+import catchRequest from "../utils/catchRequest";
+
+const createArticle = catchRequest(async (req, res, next) => {
+
     console.log(req.body);
     const article = await Article.create(req.body);
 
     res.status(201).json({
-      status: "success",
-      data: article,
+        status: "success",
+        data: article,
     });
-  } catch (err) {
-    res.status(400).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+});
 
-const getAllArticles = async (req:Request, res:Response): Promise<void> => {
-  try {
+const getAllArticles = catchRequest(async (req, res, next) => {
+
 
     const articleQuery = new APIFeatures(Article.find(), req.query)
         .filter()
@@ -32,90 +26,66 @@ const getAllArticles = async (req:Request, res:Response): Promise<void> => {
     const articles = await articleQuery.query;
 
     res.status(200).json({
-      status: "success",
-      data: articles,
+        status: "success",
+        data: articles,
     });
-  } catch (err) {
-    res.status(500).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
 
-const getArticle = async (req:Request, res:Response): Promise<void> => {
-  try {
+});
+
+const getArticle = catchRequest(async (req, res, next) => {
+
     const article = await Article.findById(req.params.id);
 
     res.status(200).json({
-      status: "success",
-      data: article,
+        status: "success",
+        data: article,
     });
-  } catch (err) {
-    res.status(500).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+
+});
 
 // const options = {
 //     projection: {_id: 0, field1: 1},
 // };
-const updateArticle = async (req:Request, res:Response): Promise<void> => {
-  try {
+const updateArticle = catchRequest(async (req, res, next) => {
+
     const article = await Article.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
+        new: true,
+        runValidators: true,
     });
 
     res.status(200).json({
-      status: "success",
-      data: article,
+        status: "success",
+        data: article,
     });
-  } catch (err) {
-    res.status(500).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
-//loh
-const deleteArticle = async (req:Request, res:Response): Promise<void> => {
-  try {
+
+});
+
+const deleteArticle = catchRequest(async (req, res, next) => {
+
     const article = await Article.findByIdAndDelete(req.params.id);
 
     res.status(204).json({
-      status: "success",
-      data: article,
+        status: "success",
+        data: article,
     });
-  } catch (err) {
-    res.status(500).json({
-      status: "fail",
-      message: err,
-    });
-  }
-};
+
+});
 
 
-const getArticleStats = async (req: Request, res: Response): Promise<void> => {
-  try{
-    const stats = await  Article.aggregate([
-      {
-        $match:{
+const getArticleStats = catchRequest(async (req, res, next) => {
 
+    const stats = await Article.aggregate([
+        {
+            $match: {}
         }
-      }
     ])
-  }catch(err){
 
-  }
-}
+})
 
 module.exports = {
-  createArticle,
-  getArticle,
-  getAllArticles,
-  updateArticle,
-  deleteArticle,
+    createArticle,
+    getArticle,
+    getAllArticles,
+    updateArticle,
+    deleteArticle,
 };
