90961151d6376beaef72f41efe460da942fa4320
Misha Oliinyk <380660775646a@gmail.com>
2024-08-03 04:22:09 +0300

    refactor user signup testing

--- src/test/UserSignUp.test.ts
+++ src/test/UserSignUp.test.ts
@@ -7,66 +7,68 @@ beforeEach(() => {
 })
 
 describe("User registration", () => {
-    test("return 200 OK when signup request is valid", (done) => {
-        request(app)
+
+    const validUser = {
+        username: "test_user",
+        email: "test_user@gmail.com",
+        password: "test_user_pwd"
+    }
+
+    const postValidUser = () => {
+        return request(app)
             .post("/users/signup")
-            .send({
-                "username": "test_user",
-                "email": "test_user@gmail.com",
-                "password": "test_user_pwd"
-            })
+            .send(validUser);
+    }
+
+    test("return 200 OK when signup request is valid", (done) => {
+        postValidUser()
             .then((res) => {
                 expect(res.status).toBe(201);
                 done();
-            })
+            });
     });
 
     test("save the user to database", (done) => {
-        request(app)
-            .post("/users/signup")
-            .send({
-                "username": "test_user",
-                "email": "test_user@gmail.com",
-                "password": "test_user_pwd"
-            })
-            .then((res) => {
-                User.find().then((userList) => {
-                    expect(userList.length).toBe(1);
-                })
-                done();
-            })
+        postValidUser()
+            .then(() => {
+                User.find()
+                    .then((userList) => {
+                        expect(userList.length).toBe(1);
+                        done();
+                    })
+            });
     });
+
     test("save the username and email to database", (done) => {
-        request(app)
-            .post("/users/signup")
-            .send({
-                "username": "test_user",
-                "email": "test_user@gmail.com",
-                "password": "test_user_pwd"
-            })
-            .then((res) => {
-                User.findOne().select("+email").then((user) => {
-                    expect(user?.username).toBe("test_user");
-                    expect(user?.email).toBe("test_user@gmail.com");
-                })
-                done();
-            })
+        postValidUser()
+            .then(() => {
+                User.findOne().select("+email")
+                    .then((user) => {
+                        expect(user?.username).toBe(validUser.username);
+                        expect(user?.email).toBe(validUser.email);
+                        done();
+                    })
+            });
     });
 
     test("return same username and email back to a client", (done) => {
-        request(app)
-            .post("/users/signup")
-            .send({
-                "username": "test_user",
-                "email": "test_user@gmail.com",
-                "password": "test_user_pwd"
-            })
+        postValidUser()
             .then((res) => {
-                expect(res.body.data.username).toBe("test_user");
-                expect(res.body.data.email).toBe("test_user@gmail.com");
-
+                expect(res.body.data.username).toBe(validUser.username);
+                expect(res.body.data.email).toBe(validUser.email);
                 done();
-            })
+            });
+    });
+
+    test("hashes the password to database", (done) => {
+        postValidUser()
+            .then(() => {
+                User.findOne().select("+password")
+                    .then((user) => {
+                        expect(user?.password).not.toBe(validUser.password);
+                        done();
+                    })
+            });
     });
 
 });
