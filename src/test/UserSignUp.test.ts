74de71ed6a25b51ee418d5e3010be7768c0005e1
Misha Oliinyk <380660775646a@gmail.com>
2024-08-05 12:51:19 +0300

    refactor user signup test and add for localization one

--- src/test/UserSignUp.test.ts
+++ src/test/UserSignUp.test.ts
@@ -2,26 +2,35 @@ import request from "supertest";
 import app from "../index";
 import User from "../models/User";
 
+const errors_en = require('./../locales/en/errors.json');
+const errors_ua = require('./../locales/ua/errors.json');
+
+import i18next from "i18next";
+
 beforeEach(() => {
     return User.deleteMany();
 })
 
+const validUser = {
+    username: "test_user",
+    email: "test_user@gmail.com",
+    password: "test_user_pwd"
+}
+
+const postUser = (userAuth: {}, options?: any) => {
+    const agent = request(app).post("/users/signup");
+
+    if(options?.language){
+        agent.set('Accept-Language', options.language)
+    }
+
+    return agent.send(userAuth);
+}
+
 describe("User registration", () => {
 
-    const validUser = {
-        username: "test_user",
-        email: "test_user@gmail.com",
-        password: "test_user_pwd"
-    }
-
-    const postValidUser = () => {
-        return request(app)
-            .post("/users/signup")
-            .send(validUser);
-    }
-
     test("return 200 OK when signup request is valid", (done) => {
-        postValidUser()
+        postUser(validUser)
             .then((res) => {
                 expect(res.status).toBe(201);
                 done();
@@ -29,7 +38,7 @@ describe("User registration", () => {
     });
 
     test("save the user to database", (done) => {
-        postValidUser()
+        postUser(validUser)
             .then(() => {
                 User.find()
                     .then((userList) => {
@@ -40,7 +49,7 @@ describe("User registration", () => {
     });
 
     test("save the username and email to database", (done) => {
-        postValidUser()
+        postUser(validUser)
             .then(() => {
                 User.findOne().select("+email")
                     .then((user) => {
@@ -52,7 +61,7 @@ describe("User registration", () => {
     });
 
     test("return same username and email back to a client", (done) => {
-        postValidUser()
+        postUser(validUser)
             .then((res) => {
                 expect(res.body.data.username).toBe(validUser.username);
                 expect(res.body.data.email).toBe(validUser.email);
@@ -61,7 +70,7 @@ describe("User registration", () => {
     });
 
     test("hashes the password to database", (done) => {
-        postValidUser()
+        postUser(validUser)
             .then(() => {
                 User.findOne().select("+password")
                     .then((user) => {
@@ -71,4 +80,62 @@ describe("User registration", () => {
             });
     });
 
+    test("return 400 when username is null", (done) => {
+        postUser({
+            username: null,
+            email: "test_user@gmail.com",
+            password: "test_user_pwd"
+        }).then((res) => {
+            expect(res.status).toBe(400);
+            done();
+        })
+    });
 });
+
+describe("Internationalization", () => {
+    test.each`
+        field         | value                                | expectedMessage
+        ${"username"} | ${null}                              | ${errors_en.validation.USERNAME_REQUIRED}
+        ${"username"} | ${"ser"}                             | ${errors_en.validation.USERNAME_MIN}
+        ${"username"} | ${"useruseruseruseruseruser"}        | ${errors_en.validation.USERNAME_MAX}
+        ${"email"}    | ${null}                              | ${errors_en.validation.EMAIL_REQUIRED}
+        ${"email"}    | ${"usergmail.com"}                   | ${errors_en.validation.BAD_EMAIL}
+        ${"email"}    | ${"ngemailuptofiftycharacterslong" +
+                          "emailuptofiftycharact@gmail.com"} | ${errors_en.validation.EMAIL_MAX}
+        ${"password"} | ${null}                              | ${errors_en.validation.PASSWORD_REQUIRED}
+        ${"password"} | ${"passwo"}                          | ${errors_en.validation.PASSWORD_MIN}
+        ${"password"} | ${"userpassworduserpassword"}        | ${errors_en.validation.PASSWORD_MAX}
+    `('when $field is $value message $expectedMessage is received when language is set as english', ({field, value, expectedMessage}, done) => {
+        const user: any = {...validUser};
+        user[field] = value;
+
+        postUser(user, {language: "en"})
+            .then((res) => {
+                expect(res.body.message).toBe(expectedMessage);
+                done();
+            });
+    });
+
+    test.each`
+        field         | value                                | expectedMessage
+        ${"username"} | ${null}                              | ${errors_ua.validation.USERNAME_REQUIRED}
+        ${"username"} | ${"ser"}                             | ${errors_ua.validation.USERNAME_MIN}
+        ${"username"} | ${"useruseruseruseruseruser"}        | ${errors_ua.validation.USERNAME_MAX}
+        ${"email"}    | ${null}                              | ${errors_ua.validation.EMAIL_REQUIRED}
+        ${"email"}    | ${"usergmail.com"}                   | ${errors_ua.validation.BAD_EMAIL}
+        ${"email"}    | ${"ngemailuptofiftycharacterslong" +
+                          "emailuptofiftycharact@gmail.com"} | ${errors_ua.validation.EMAIL_MAX}
+        ${"password"} | ${null}                              | ${errors_ua.validation.PASSWORD_REQUIRED}
+        ${"password"} | ${"passwo"}                          | ${errors_ua.validation.PASSWORD_MIN}
+        ${"password"} | ${"userpassworduserpassword"}        | ${errors_ua.validation.PASSWORD_MAX}
+    `('when $field is $value message $expectedMessage is received when language is set as ukraine', ({field, value, expectedMessage}, done) => {
+        const user: any = {...validUser};
+        user[field] = value;
+
+        postUser(user, {language: "ua"})
+            .then((res) => {
+                expect(res.body.message).toBe(expectedMessage);
+                done();
+            });
+    });
+})
